15728ab6339b3f91f559f943c478b9a325d535bb
DiegoAgudelo4 <diegoagudelo180@gmail.com>
2025-03-04 11:40:47 -0500

    CRUD for candidates with swagger documentation

---
+++ src/models/Candidate.js
@@ -0,0 +1,40 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/db');
+
+const Voter = sequelize.define(
+  'Candidate',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    party: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    votes: {
+      type: DataTypes.BIGINT,
+      allowNull: true,
+    },
+    available: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+  },
+  {
+    tableName: 'candidate',
+    timestamps: false,
+  },
+);
+
+module.exports = Voter;
--- src/routes/candidateRoutes.js
+++ src/routes/candidateRoutes.js
@@ -1,9 +1,133 @@
 const express = require('express');
 
 const router = express.Router();
+const { getCandidates, createCandidate, getCandidateById, deleteCandidate } = require('../controllers/candidateController');
 
-router.get('/', (req, res) => {
-  res.json({ message: 'Lista de candidatos' });
-});
+/**
+ * @swagger
+ * /candidates:
+ *   get:
+ *     summary: Obtiene la lista de candidatos con paginación
+ *     tags: [Candidates]
+ *     parameters:
+ *       - in: query
+ *         name: page
+ *         schema:
+ *           type: integer
+ *         description: Número de la página (por defecto 1)
+ *       - in: query
+ *         name: limit
+ *         schema:
+ *           type: integer
+ *         description: Cantidad de candidatos por página (por defecto 10)
+ *     responses:
+ *       200:
+ *         description: Lista de candidatos obtenida con éxito.
+ *       500:
+ *         description: Error al obtener los candidatos.
+ *   post:
+ *     summary: Registra un nuevo candidato
+ *     tags: [Candidates]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *               - email
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: Nombre del candidato
+ *               email:
+ *                 type: string
+ *                 format: email
+ *                 description: Correo electrónico único del candidato
+ *     responses:
+ *       201:
+ *         description: candidato registrado con éxito.
+ *       400:
+ *         description: Datos de entrada inválidos.
+ *       500:
+ *         description: Error al registrar el candidato.
+ */
+
+router.get('/', getCandidates);
+router.post('/', createCandidate);
+
+/**
+ * @swagger
+ * /candidates/{id}:
+ *   get:
+ *     summary: Obtiene un candidato por ID
+ *     tags: [Candidates]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID del candidato
+ *     responses:
+ *       200:
+ *         description: Datos del candidato obtenidos con éxito.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Candidate'
+ *       404:
+ *         description: candidato no encontrado.
+ *       500:
+ *         description: Error al obtener el candidato.
+ *   delete:
+ *     summary: Eliminación lógica de un candidato (available=false)
+ *     tags: [Candidates]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID del candidato a eliminar
+ *     responses:
+ *       200:
+ *         description: candidato eliminado correctamente.
+ *       404:
+ *         description: candidato no encontrado.
+ *       500:
+ *         description: Error al eliminar el candidato.
+ */
+
+router.get('/:id', getCandidateById);
+router.delete('/:id', deleteCandidate);
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Candidate:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: integer
+ *           description: ID único del candidato
+ *         name:
+ *           type: string
+ *           description: Nombre del candidato
+ *         email:
+ *           type: string
+ *           format: email
+ *           description: Correo electrónico único del candidato
+ *         party:
+ *           type: string
+ *           description: Partido al que pertenece el candidato (Opcional)
+ *       example:
+ *         id: 1
+ *         name: "Juan Pérez"
+ *         email: "juan@example.com"
+ *         party: "partido example"
+ */
 
 module.exports = router;
